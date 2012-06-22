92137591b5044c3f824bedeef36cd25fdcd359a8
lepture <lepture@me.com>
2012-06-22 13:15:56 +0800

    tagcloud support

--- filters.py
+++ filters.py
@@ -5,7 +5,8 @@ def tag_url(tag):
     from liquidluck.writers.base import content_url
     from liquidluck.options import settings
     prefix = settings.site.get('prefix', '')
-    return content_url(prefix, 'tag', tag, 'index.html')
+    root = content_url(prefix, 'tag', 'index.html')
+    return '%s#%s' % (root, tag)
 
 
 def year_url(post):
