240890b9e5b0d06f706575b21f10a827fe8145ad
lepture <lepture@me.com>
2012-09-12 18:18:39 +0800

    update for Felix Felicis 3.0

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 name = 'moment'
-version = '0.10'
+version = '0.11'
 website = 'https://github.com/lepture/liquidluck-theme-moment'
 author = {
     'name': 'Hsiaoming Yang',
@@ -12,8 +12,8 @@ author = {
 lang = 'en'
 
 navigation = [
-    ('Home', '/'),
-    ('About', '/about.html'),
+    {'title': 'Home', 'link': '/'},
+    {'title': 'About', 'link': '/about.html'},
 ]
 
 twitter = 'lepture'
